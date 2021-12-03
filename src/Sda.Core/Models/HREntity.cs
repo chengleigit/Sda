bfc7ebda7d311143329a83bc62d499e97dc14dcc
chenglei <cheng.lei@synyi.com>
2021-12-03 14:00:12 +0800

    添加项目文件。

---
+++ src/Sda.Core/Models/HREntity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sda.Core.Models
+{
+    public class HREntity
+    {
+        public Guid Id { get; set; }
+        public Guid ConcurrencyStamp { get; set; }
+        /// <summary>
+        /// 比赛Id
+        /// </summary>
+        public Guid BattleId { get; set; }
+        /// <summary>
+        /// 是否结束
+        /// </summary>
+        public bool IsOver { get; set; }
+        /// <summary>
+        /// 步长
+        /// </summary>
+        public int CurTime { get; set; }
+
+
+
+
+    }
+}
